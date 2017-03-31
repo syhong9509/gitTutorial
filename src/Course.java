76d6962d0de530e82d28c700787c8510fcff457d
S. Hong <shong257@uwo.ca>
2017-03-31 16:23:33 -0400

    gitTutorial

--- src/Course.java
+++ src/Course.java
@@ -2,4 +2,11 @@
  * Created by syhon on 2017-03-31.
  */
 public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
 }
